10314cc4ff9945894b9eeef889664198183fee6f
Ricael Chiquetti <ricaelchiquetti27@gmail.com>
2025-02-08 18:40:20 -0300

    first commit

---
+++ database/migrations/2025_02_02_125458_create_finance_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('finance_transactions', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type', ['receivable', 'payable']);
+
+            $table->date('due_date');
+            $table->date('transaction_date')->nullable();
+
+            $table->decimal('value', 10, 2);
+            $table->string('payment_method')->nullable();
+
+            $table->foreignId('entity_id')->constrained('entities')->onDelete('cascade'); 
+            $table->foreignId('company_id')->constrained()->onDelete('cascade');
+            $table->foreignId('account_plan_id')->constrained('account_plans')->onDelete('cascade'); 
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('finance_transactions');
+    }
+};
