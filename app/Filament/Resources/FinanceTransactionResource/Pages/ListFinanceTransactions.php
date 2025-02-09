167f2c1b3bc810e05031f6fde770415a81f34a70
Ricael Chiquetti <ricaelchiquetti27@gmail.com>
2025-02-09 10:55:41 -0300

    fix/add trait

--- app/Filament/Resources/FinanceTransactionResource/Pages/ListFinanceTransactions.php
+++ app/Filament/Resources/FinanceTransactionResource/Pages/ListFinanceTransactions.php
@@ -4,10 +4,13 @@ namespace App\Filament\Resources\FinanceTransactionResource\Pages;
 
 use App\Filament\Resources\FinanceTransactionResource;
 use Filament\Actions;
+use Filament\Pages\Concerns\ExposesTableToWidgets;
 use Filament\Resources\Pages\ListRecords;
 
 class ListFinanceTransactions extends ListRecords
 {
+    use ExposesTableToWidgets;
+
     protected static string $resource = FinanceTransactionResource::class;
 
     protected function getHeaderActions(): array
