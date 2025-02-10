554cc6c1eba59b946416b6dc8c39e5aed2c22081
Ricael Chiquetti <ricaelchiquetti27@gmail.com>
2025-02-10 19:40:49 -0300

    fix

--- build.sh
+++ build.sh
@@ -2,14 +2,13 @@
 # Make sure this file has executable permissions, run `chmod +x build.sh`
 
 # Build assets using NPM
-./vendor/bin/sail npm install
-./vendor/bin/sail npm run build
+npm run build
 
 # Clear cache
-./vendor/bin/sail  artisan optimize:clear
+php artisan optimize:clear
 
 # Cache the various components of the Laravel application
-./vendor/bin/sail  artisan config:cache
-./vendor/bin/sail  artisan event:cache
-./vendor/bin/sail  artisan route:cache
-./vendor/bin/sail  artisan view:cache
+php artisan config:cache
+php artisan event:cache
+php artisan route:cache
+php artisan view:cache
